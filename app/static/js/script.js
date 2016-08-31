e2e8b5cadbceb8d5fa9fb15e97a4f688aa2664a8
canhtran <a0134553@u.nus.edu>
2016-08-31 18:54:34 +0800

    Add update DB button

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -18,4 +18,24 @@ $( document ).ready(function(){
             },
         });
     });
+
+    $('#addPlayer').submit(function(event){
+        event.preventDefault();
+        var postData = $(this).serializeArray();
+	    var formURL = $(this).attr("action");
+        $('body').addClass('loaded');
+        $.ajax({
+            type:'post',
+            url : formURL,
+            data: postData,
+            success: function(result) {
+                if (result == 'done') {
+                    window.location.href = "/";
+                } else {
+                    $('body').removeClass('loaded');
+                    alert(result);
+                }
+            },
+        });
+    });
 });
